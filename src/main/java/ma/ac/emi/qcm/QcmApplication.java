dcf18200e264ab86c7775e1790fcb9f59f8af8ab
Oussama Siwane <oussamasiwane@student.emi.ac.ma>
2020-06-24 14:18:40 +0100

    commit

--- src/main/java/ma/ac/emi/qcm/QcmApplication.java
+++ src/main/java/ma/ac/emi/qcm/QcmApplication.java
@@ -105,7 +105,6 @@ public class QcmApplication {
 			reponseRepository.save(new Reponse("The first question", true, question1));
 			reponseRepository.save(new Reponse("The second question", false, question1));
 			reponseRepository.save(new Reponse("The third question", true, question1));
-			reponseRepository.save(new Reponse("The third question", true, question1));
 
 		};
 	}
